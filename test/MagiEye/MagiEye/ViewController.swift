54951eed59e89c40168057b248bf31e495bf90bc
AnRanScheme <541437674@qq.com>
2019-03-15 10:00:53 +0800

    添加例子

---
+++ test/MagiEye/MagiEye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagiEye
+//
+//  Created by 安然 on 2019/3/15.
+//  Copyright © 2019 anran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
